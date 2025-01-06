9b7796411b087d730dbef5a692da5d8214da6783
Intune & Vita Doctrina <bryntze.cloud@hotmail.com>
2025-01-06 12:07:00 +0100

    Create JBNWebhookAddDeviceToEntraIDGroupBasedOnFirefoxInstalled.ps1
    Version 1

---
+++ JBNWebhookAddDeviceToEntraIDGroupBasedOnFirefoxInstalled.ps1
@@ -0,0 +1,19 @@
+### PowerShell script to be used to check an application (default 64-bit Mozilla Firefox, but can change variable on line 10 $JBNAppPath to another app.
+### Explained in YouTube video from: https://www.youtube.com/@IntuneVitaDoctrina where we create the Webhook URI needed in line 7
+### Author: John Bryntze
+### Date: 6th January 2024
+
+$JBNparams  = @{  
+DeviceName ="$env:COMPUTERNAME"
+} 
+$JBNwebhookURI = ""
+$JBNAppPath = "C:\Program Files\Mozilla Firefox\firefox.exe"
+
+if (test-path -path $JBNAppPath) {
+    Write-Host "Firefox is found at $JBNAppPath - add $env:COMPUTERNAME to group"
+    # Call the webhook
+    $JBNbody = ConvertTo-Json -InputObject $params  
+    $JBNresponse = Invoke-WebRequest -Method Post -Uri $JBNwebhookURI -Body $JBNbody -UseBasicParsing 
+} else {
+    Write-Host "Firefox is not found at $JBNAppPath. Do not add $env:COMPUTERNAME to group"
+} 
